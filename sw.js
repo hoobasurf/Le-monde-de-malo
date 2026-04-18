01fd9ad7d93373ad513b22cd43dfd49e9b8c58ca
hoobasurf <brice.paneela@icloud.com>
2026-04-18 11:59:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,18 @@
-const CACHE_NAME = ‘malo-world-v1’;
+const CACHE = ‘malo-v1’;
 const ASSETS = [
-‘/’,
-‘/index.html’,
-‘/manifest.json’,
-‘/js/melody.js’,
-‘/games/bulles.html’,
-‘/games/animaux.html’,
-‘/games/puzzle.html’,
-‘/games/comptines.html’,
-‘/games/marin.html’,
-‘/games/cuisine.html’,
-‘/games/tracteur.html’
+‘index.html’, ‘manifest.json’, ‘melody.js’,
+‘bulles.html’, ‘animaux.html’, ‘puzzle.html’,
+‘comptines.html’, ‘marin.html’, ‘cuisine.html’, ‘tracteur.html’
 ];
 
 self.addEventListener(‘install’, e => {
-e.waitUntil(
-caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-);
+e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS.map(a => new Request(a, {cache:‘reload’})))).catch(()=>{}));
 self.skipWaiting();
 });
-
 self.addEventListener(‘activate’, e => {
-e.waitUntil(
-caches.keys().then(keys =>
-Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-)
-);
+e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
 self.clients.claim();
 });
-
 self.addEventListener(‘fetch’, e => {
-e.respondWith(
-caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match(’/index.html’)))
-);
+e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match(‘index.html’))));
 });
